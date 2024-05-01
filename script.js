d1c523eb3f9d7d9ed30b79dedc766858fc980568
John Vincent Lizada <91546656+Keshinn9@users.noreply.github.com>
2024-05-01 11:36:12 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-
+function myFunction() {
+    var x = document.querySelector(".navbar");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
 

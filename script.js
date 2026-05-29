d35cd2716122420a44dd1b595df762fe0f2d6481
Keshinn9 <jvlizada8@gmail.com>
2026-05-29 23:01:45 +0800

    feat: implemented modal for portfolio page, not complete

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 
 
+const portfolioBtn = document.getElementsByClassName("more_btn");
+const portfolioItemsDialog = document.getElementById("portfolio_items_popUp");
+
+for (let btn of portfolioBtn){ //loop through all btn with the class name of more_btn because its not only one so its needed
+    btn.addEventListener("click", function () {
+        portfolioItemsDialog.showModal();
+    });
+}
+
+
